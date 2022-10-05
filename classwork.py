22630adc760faa7aeef118911c0247323686540a
shina-oj <32014066+shina-oj@users.noreply.github.com>
2022-10-05 11:46:02 +0100

    Add files via upload

--- classwork.py
+++ classwork.py
@@ -1,18 +1,58 @@
-#find x if 4= 2x + 5
-x = (4-5)/2
+#how many seconds are there in 42 minutes 42 seconds
+
+one_minute = 60
+minute_given= 42
+second_given = 42
+answer = (one_minute*minute_given) + second_given
+print(answer)
+
+#miles in 10 kilometers
+one_mile= 1.61
+ten_kilometer = 10
+solution =ten_kilometer/one_mile
+print(solution)
+
+#average time per miles in minutes
+run_time_in_km= 10
+minute_ran=42
+seconds_ran=42
+run_time_in_miles= run_time_in_km/one_mile
+
+
+
+
+
+
+#exercise 2.1
+n=42
+print(42-n)
+
+x=y=1
 print(x)
+print(y)
 
-#find the area of a triangle using herons formula if a=5, b=7 c= 10
-a=5
-b=7
-c=10
-s=(a+b+c)/2
-area= (s*(s-a)*(s-b)*(s-c))**0.5
-#simplifying
+#what happens when you put semi colon at the end of a python statement
+a=5 ;
+print(a)
+#it doesnt affect the statement
 
-j=(s-a)
-k=(s-b)
-l=(s-c)
-m=(s*j*k*l)
-n=m**0.5
-print(n)
+#print xy
+print("you get a name error")
+
+#exercise2.2
+pi=3.142
+r=5
+volume=(1.33*pi*r**3)
+print(volume)
+
+#total wholesale cost
+book_price= 24.95
+discount_price= 0.40*24.95
+first_copy_shipping= 3
+other_shipping= 0.75*59
+total_price= discount_price*60
+total_shipping= first_copy_shipping + other_shipping
+wholesale_cost=total_price+total_shipping
+print(wholesale_cost)
+
+
